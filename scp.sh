3c42353eb212fccf1382387048c1cf46354598b6
flederwiesel <flederwiesel@fra-flugplan.de>
2020-06-03 22:04:09 +0200

    Remove unnecessary `find`.

--- scp.sh
+++ scp.sh
@@ -6,8 +6,6 @@ script="${BASH_SOURCE[0]}"
 scriptdir=$(dirname "$script")
 cd "$scriptdir"
 
-find -not -perm u=rwx,g=rx,o=rx -a -not -perm u=rw,g=r,o=r
-
 rsync -rtv \
 	--chmod=a+rw,g+rw,o+r \
 	--exclude=*.sh \
