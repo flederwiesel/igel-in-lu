0cec7b8aacdb64b477f29071e45ae40ac4b84a95
flederwiesel <flederwiesel@fra-flugplan.de>
2020-06-13 01:24:04 +0200

    Prior upload, check whether marker images exist.

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,29 @@ script="${BASH_SOURCE[0]}"
 scriptdir=$(dirname "$script")
 cd "$scriptdir"
 
+[ "$(uname -s)" = "Linux" ] || protocol=--protocol=TCP
+
+while read marker
+do
+	[ -e "img/$marker.png" ] ||
+	{
+		echo "img/$marker.png not found." >&2
+		exit 1
+	}
+done < <(
+mysql $protocol \
+	--user=igelhilfe \
+	--password="$(getpass machine=mysql://localhost login=igelhilfe)" \
+	--database "igel-in-lu" \
+	--default-character-set=utf8 \
+	--skip-column-names \
+	<<"EOF"
+		SELECT DISTINCT CONCAT(`marker1`, '-', `marker2`) AS `marker`
+		FROM `hedgehogs`
+		ORDER BY `marker`
+EOF
+)
+
 rsync -rtv \
 	--chmod=a+rw,g+rw,o+r \
 	--exclude=.git* \
