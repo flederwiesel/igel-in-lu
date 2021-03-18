f8089778f95bcecc1f1736c44eec1ba9e5af0fe3
flederwiesel <flederwiesel@fra-flugplan.de>
2021-03-18 22:44:49 +0100

    Indent within `if (...)`.

--- api/v0.1/colours.php
+++ api/v0.1/colours.php
@@ -6,37 +6,35 @@ require_once("lib/database.php");
 
 if ("GET" == strtoupper("$_SERVER[REQUEST_METHOD]"))
 {
-
-$query = <<<SQL
-	SELECT
-		`name`,
-		`value`
-	FROM `colours`
-	ORDER BY `weight` ASC
+	$query = <<<SQL
+		SELECT
+			`name`,
+			`value`
+		FROM `colours`
+		ORDER BY `weight` ASC
 SQL;
 
-try
-{
-	$st = $db->prepare($query);
-	$st->execute();
+	try
+	{
+		$st = $db->prepare($query);
+		$st->execute();
 
-	echo json_encode($st->fetchAll());
-}
-catch (Exception $e)
-{
-	$date = date("c");
-	$uuid = uuid();
+		echo json_encode($st->fetchAll());
+	}
+	catch (Exception $e)
+	{
+		$date = date("c");
+		$uuid = uuid();
 
-	syslog(LOG_EMERG,
-		   "Exception ticket $uuid [$date]:\n".
-		   "{$e->getCode()}: {$e->getMessage()}\n".
-		   "{$e->getTraceAsString()}");
+		syslog(LOG_EMERG,
+			   "Exception ticket $uuid [$date]:\n".
+			   "{$e->getCode()}: {$e->getMessage()}\n".
+			   "{$e->getTraceAsString()}");
 
-	echo json_encode([
-		"error" => "A database error occured. For support, refer to ticket $uuid."
-	]);
-}
-
+		echo json_encode([
+			"error" => "A database error occured. For support, refer to ticket $uuid."
+		]);
+	}
 }
 
 ?>
